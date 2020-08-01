04993e8d7b2e6247bff25c1359fc1d6e53e799a1
Meneksekaraoglu <meneksekaraoglu@msn.com>
2020-08-01 09:56:17 -0600

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,9 +2,9 @@ import java.sql.SQLOutput;
 
 public class HappyB20 {
     public static void main(String[] args) {
-        System.out.println("B20 is always happy");
+
 
         System.out.println("I made changes");
-
+//added a new line
     }
 }
